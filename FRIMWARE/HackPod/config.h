caaa19317330b45078b27fea60a67051417aad01
wenfeng110402 <wenfeng110402@icloud.com>
2025-11-23 14:15:45 +0800

    add files

--- FRIMWARE/HackPod/config.h
+++ FRIMWARE/HackPod/config.h
@@ -2,12 +2,24 @@
 #define CONFIG_H
 
 // 1.i2s dac
-#define I2S_BCLK_PIN 
-#define I2S_LRCK_PIN 
-#define I2S_DOUT_PIN     //data out pin
+#define I2S_BCK_PIN 12
+#define I2S_LRCK_PIN 11
+#define I2S_DATA_PIN 10    //data out pin
 
 // 2.sdcard
-#define SD_MMC_CLK_PIN
-#define SD_MMC_CMD_PIN
-#define SD_MMC_D0_PIN
+#define SD_CLX_PIN 46
+#define SD_CMD_PIN 45
+#define SD_D0_PIN 38
+#define SD_CD_PIN 37
+
+// 3.buttons
+#define B_PP_PIN 4 //play/pause
+#define B_NEXT_PIN 6
+#define B_PREV_PIN 5
+#define B_VU_PIN 7
+#define B_VD_PIN 8
+#define B_HACK_PIN 9
+
+// 4.BattVolt ADC
+#define BAT_ADC_PIN 
 
